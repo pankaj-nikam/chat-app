7f7078a13665b48d25c5216cf358188714eadf7a
Pankaj Nikam <pankaj-nikam@users.noreply.github.com>
2022-03-13 08:50:14 +0530

    Added static files and completed handling messages.

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
-use rocket::{
-    serde::{Deserialize, Serialize},
-    tokio::sync::broadcast::channel,
-};
-
 #[macro_use]
 extern crate rocket;
 
-#[get("/world")]
-fn world() -> &'static str {
-    "Hello Pankaj"
-}
+use rocket::form::Form;
+use rocket::fs::{relative, FileServer};
+use rocket::response::stream::{Event, EventStream};
+use rocket::serde::{Deserialize, Serialize};
+use rocket::tokio::select;
+use rocket::tokio::sync::broadcast::{channel, error::RecvError, Sender};
+use rocket::{Shutdown, State};
 
 #[derive(Debug, Clone, FromForm, Serialize, Deserialize)]
 #[serde(crate = "rocket::serde")]
@@ -21,9 +19,35 @@ struct Message {
     pub message: String,
 }
 
+#[post("/message", data = "<form>")]
+fn post(form: Form<Message>, queue: &State<Sender<Message>>) {
+    //If there are no subscribers, the send method fails.
+    let _res = queue.send(form.into_inner());
+}
+
+#[get("/events")]
+async fn events(queue: &State<Sender<Message>>, mut end: Shutdown) -> EventStream![] {
+    let mut rx = queue.subscribe();
+    EventStream! {
+        loop {
+            let msg = select! {
+                msg = rx.recv() => match msg {
+                    Ok(msg) => msg,
+                    Err(RecvError::Closed) => break,
+                    Err(RecvError::Lagged(_)) => continue,
+                },
+                _ = &mut end => break,
+            };
+
+            yield Event::json(&msg);
+        }
+    }
+}
+
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .manage(channel::<Message>(1024).0)
-        .mount("/hello", routes![world])
+        .mount("/", routes![post, events])
+        .mount("/", FileServer::from(relative!("static")))
 }
